0469aba8c39a8086fdf08f68a8aeb1c06d640c43
Jan Wojnar <jan.wojnar@capgemini.com>
2022-06-20 15:56:10 +0200

    page improvements

--- project-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "./recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'test', 'https://www.acouplecooks.com/wp-content/uploads/2021/03/' +
+      'Cheese-Tortellini-011-735x919.jpg'),
+    new Recipe('A Test Recipe', 'test', 'https://www.acouplecooks.com/wp-content/uploads/2021/03/' +
+      'Cheese-Tortellini-011-735x919.jpg')
+  ];
 
   constructor() { }
 
