4ad7e96c29cf1799ac232c49d9dcc9761222c09c
Jan Wojnar <jan.wojnar@capgemini.com>
2022-07-27 12:34:48 +0200

    pipes, subjects learned!

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from './user/user.service';
+import {Subscription} from 'rxjs';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  userActivated = false;
+  private activeSub: Subscription;
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit() {
+    this.activeSub = this.userService.activatedEmitter.subscribe((didActivate:boolean) => {
+      this.userActivated=didActivate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activeSub.unsubscribe();
+  }
 }
