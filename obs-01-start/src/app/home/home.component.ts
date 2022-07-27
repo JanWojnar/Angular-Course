392cb888fde40af0066395d828db39c3ca3a114f
Jan Wojnar <jan.wojnar@capgemini.com>
2022-07-27 12:14:21 +0200

    custom observables trained!

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Subscription} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private firstObsSubscription: Subscription;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'))
+        }
+        count++;
+      }, 1000);
+    })
+
+    const transformedObservable = customIntervalObservable.pipe(
+      filter((data:number) => {
+        return data > 0;
+      }),
+      map((data:number) => {
+      return 'Round: '+(data+1);
+    }));
+
+    this.firstObsSubscription = transformedObservable.subscribe(
+      data => {
+        console.log(data);
+      }, error => {
+        alert(error.message);
+      }, () => {
+        console.log('Job done!');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
