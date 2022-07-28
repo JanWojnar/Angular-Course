220aee4affc408fd09038a085132aeb9bf214109
Jan Wojnar <jan.wojnar@capgemini.com>
2022-07-28 11:43:11 +0200

    task done

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('f') registerForm: NgForm;
+  defaultSubType: string = 'Advanced';
+
+  subTypes: string[] = ['Basic','Advanced','Pro'];
+
+  submitted: boolean = false;
+
+  accData = {
+    email: '',
+    subType: '',
+    password: ''
+  }
+
+  onSubmit(){
+    console.log(this.registerForm);
+    this.accData.email = this.registerForm.form.value.email;
+    this.accData.subType = this.registerForm.form.value.subscription;
+    this.accData.password = this.registerForm.form.value.password;
+    this.submitted=true;
+  }
 }
