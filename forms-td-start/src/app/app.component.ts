193039c8c38d59b7eca7288141fd5f002a62b163
Jan Wojnar <jan.wojnar@capgemini.com>
2022-07-28 09:32:44 +0200

    Template-DrivenForm part done!

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,16 +9,49 @@ import {NgForm} from "@angular/forms";
 export class AppComponent {
 
   @ViewChild('f') signupForm: NgForm; //approach2
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted=false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    //   })
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(form: NgForm){   //approach1
   //   console.log(form);
   // }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);  //approach2
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted=true;
+
+    this.signupForm.reset();
   }
 }
