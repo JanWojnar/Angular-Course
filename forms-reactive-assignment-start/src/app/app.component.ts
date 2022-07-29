08df0ff8271f4d1eaf2edc96b3a612052c53e6d0
Jan Wojnar <jan.wojnar@capgemini.com>
2022-07-29 10:15:33 +0200

    Task done!

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup;
+  statuses: string[] = ['Stable', 'Critical', 'Finished']
+  forbiddenProjectNames: string[] = ['Test']
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,[Validators.required],[this.forbiddenProjectName.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(null)
+    })
+  }
+
+  onSubmit(){
+    console.log(this.projectForm);
+  }
+
+  forbiddenProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(this.forbiddenProjectNames.indexOf(control.value)!==-1){
+          resolve({'projectNameForbidden' : true});
+        } else {
+          resolve(null);
+        }
+      },1500)
+    })
+    return promise;
+  }
+
 }
