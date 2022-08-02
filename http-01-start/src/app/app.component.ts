207cd8f8cdd2a74f3e6d2b154d3f9dcb7e6dd965
Jan Wojnar <jan.wojnar@capgemini.com>
2022-08-02 15:50:08 +0200

    http angular basic post responses handling

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,28 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
+import {Post} from "./post.model";
+import {PostService} from "./post.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(private http: HttpClient) {}
+  loadedPosts: Post[] = [];
+  isFetching: boolean = false;
+  error = null;
+  private errorSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private http: HttpClient, private postService: PostService) {
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
+  ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(errMess => {
+      this.error = errMess;
+    })
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.loadedPosts = posts;
+      this.isFetching = false
+    }, error => {
+      this.error = error.message;
+    })
+  }
+
+  onCreatePost(postData: Post) {
+    this.postService.createAndStorePosts(postData);
     console.log(postData);
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.isFetching = true;
+    this.postService.fetchPosts()
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+        this.isFetching=false;
+      }, error => {
+        this.error = error.message;
+        this.isFetching=false;
+      })
   }
 
   onClearPosts() {
-    // Send Http request
+    this.postService.deletePosts().subscribe(() => {
+      this.postService.fetchPosts().subscribe(posts => {
+        this.loadedPosts = posts;
+      })
+    });
+  }
+
+  onHandleError() {
+    this.error=null;
+    this.isFetching=false;
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
---
+++ http-01-start/src/app/post.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Post} from "./post.model";
+import {catchError, map} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) {
+  }
+
+  createAndStorePosts(post: Post) {
+    this.http.post<{ name: string }>(
+      'https://ng-complete-guide-f378b-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      post).subscribe(responseData => {
+      console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
+    });
+  }
+
+  fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('key','value');
+    searchParams = searchParams.append('keyZZZ','valueZZZ');
+    return this.http.get<{ [key: string]: Post }>(
+      'https://ng-complete-guide-f378b-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      {
+        headers: new HttpHeaders({'Custom-Header': 'Hello!'}),
+        // params: new HttpParams().set('print','pretty')
+        params: searchParams
+      }
+    )
+      .pipe(map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key});
+            }
+          }
+          return postsArray;
+        }), catchError(errRes => {
+          //Sending to analytic server etc
+          return throwError(errRes);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http.delete('https://ng-complete-guide-f378b-default-rtdb.europe-west1.firebasedatabase.app/posts.json');
+  }
+}
