8889fede883c5c64875627f3dae94749458e8d8d
Jan Wojnar <jan.wojnar@capgemini.com>
2022-08-11 15:00:42 +0200

    Recipies send to separate module

--- project-app/src/app/app-routing.module.ts
+++ project-app/src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipsesResolverService } from "./recipes/recipses-resolver.service";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {AuthComponent} from "./auth/auth.component";
-import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
   {path: 'auth', component: AuthComponent},
   {path: '', redirectTo: '/shopping-list', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
-    {path: '', component: RecipeStartComponent},
-    {path: 'new', component: RecipeEditComponent},
-    {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipsesResolverService]},
-    {path: ':id', component: RecipeDetailComponent, resolve: [RecipsesResolverService]}
-  ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
 
