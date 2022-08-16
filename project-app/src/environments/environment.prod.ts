ddae4da2234b08b31393a69c22a9b32a813a2b8d
Jan Wojnar <jan.wojnar@capgemini.com>
2022-08-16 09:02:26 +0200

    deploy state

--- project-app/src/environments/environment.prod.ts
+++ project-app/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  firbaseAPIKey: 'AIzaSyA8kIjNtyFk1jGqbqqtURNS6kRT8xWs3Bs'
+
 };
--- project-app/src/environments/environment.ts
+++ project-app/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firbaseAPIKey: 'AIzaSyA8kIjNtyFk1jGqbqqtURNS6kRT8xWs3Bs'
 };
 
 /*
