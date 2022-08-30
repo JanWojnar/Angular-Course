1fc31ba1c76fc77be1d09579513ab26ff7f4d24b
Jan Wojnar <jan.wojnar@capgemini.com>
2022-08-30 15:41:31 +0200

    data sotrage service cleanup

--- project-app/src/app/shared/data-storage/data-storage.service.ts
+++ project-app/src/app/shared/data-storage/data-storage.service.ts
@@ -1,13 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../../recipes/recipe.service";
-import {Recipe} from "../../recipes/recipe-list/recipe.model";
-import {map, tap} from "rxjs/operators";
+import {map} from "rxjs/operators";
 import {AuthService} from "../../auth/auth.service";
 import {Subscription} from "rxjs";
 import {Store} from "@ngrx/store";
 import {AppState} from "../store/app-state";
-import * as RecipeActions from "../../recipes/store/recipe.actions";
 
 
 @Injectable({
@@ -25,41 +23,6 @@ export class DataStorageService {
           this.token = user.token;
         }
       }
-    )
-  }
-
-  fetchRecipes() {
-    // return this.authService.user.pipe(
-    //   take(1),
-    //   exhaustMap(user => {
-    //     return this.http.get<Recipe[]>('https://recipebook-2b443-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-    //       {
-    //         params: new HttpParams().set('auth', <string>user.token)
-    //       })
-    //   }),
-    //   map(recipes => {
-    //     return recipes.map(recipe => {
-    //       return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-    //     });
-    //   }),
-    //   tap(recipes => {
-    //     this.recipeService.setRecipes(recipes);
-    //   })
-    // );
-    return this.http.get<Recipe[]>('https://recipebook-2b443-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
-      // {
-      //   params: new HttpParams().set('auth', <string>this.token)
-      // }
-    ).pipe(
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-      }),
-      tap(recipes => {
-        // this.recipeService.setRecipes(recipes);
-        this.store.dispatch(new RecipeActions.SetRecipes(recipes));
-      })
     )
   }
 }
